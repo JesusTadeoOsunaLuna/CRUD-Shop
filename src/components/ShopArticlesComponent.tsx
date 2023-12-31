bd818d59d52c5e8175c23623463efc1946a42bb4
JesusTadeoOsunaLuna <l19330633@hermosillo.tecnm.mx>
2023-12-31 02:56:12 -0700

    stuff

--- src/components/ShopArticlesComponent.tsx
+++ src/components/ShopArticlesComponent.tsx
@@ -1,38 +1,34 @@
-import { useSetData } from '../hooks/useFetch'
-import { LoadingSpinner } from '../components/LoadingSpinner'
-import { useNavigate } from 'react-router-dom'
+import { useSetData } from "../hooks/useFetch";
+import { LoadingSpinner } from "../components/LoadingSpinner";
+import { useNavigate } from "react-router-dom";
 
 export const ShopArticlesComponent = () => {
-    const data = useSetData()
-    const navigate = useNavigate()
-    console.log({ data: data })
-    return (
-        <article className="bg-slate-300 flex justify-center">
-            <ul>
-                {!data ? (
-                    <LoadingSpinner />
-                ) : (
-                    data.map((data, index) => (
-                        <button
-                            key={index}
-                            onClick={() => navigate(`/items/${data.id}`)}
-                        >
-                            <li className="m-4 mb-10  flex justify-start">
-                                <img
-                                    className="h-auto w-60 max-w-sm border"
-                                    src={data.thumbnail}
-                                />
-                                <div className="flex flex-col">
-                                    <p className="text-lg font-bold ">
-                                        {data.title}
-                                    </p>
-                                    <p>{data.description}</p>
-                                </div>
-                            </li>
-                        </button>
-                    ))
-                )}
-            </ul>
-        </article>
-    )
-}
+  const data = useSetData();
+  const navigate = useNavigate();
+  console.log({ data: data });
+
+  if (!data) return <LoadingSpinner />;
+
+  return (
+    <article className="bg-slate-300 flex justify-start">
+      <ul>
+        {data.map((data, index) => (
+          <div key={index} className="flex">
+            <button onClick={() => navigate(`/items/${data.id}`)}>
+              <li className="m-4 mb-10  flex justify-start">
+                <img
+                  className="h-auto w-60 max-w-sm border"
+                  src={data.thumbnail}
+                />
+                <div className="flex flex-col">
+                  <p className="text-lg font-bold ">{data.title}</p>
+                  <p>{data.description}</p>
+                </div>
+              </li>
+            </button>
+          </div>
+        ))}
+      </ul>
+    </article>
+  );
+};
